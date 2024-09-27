1521ef5cfc0d274aa9d8cc2fea5f625e38b1a967
Esther-Naserian <naseriansamuria@gmail.com>
2024-09-27 12:32:13 +0300

    added homepage

--- naji-dashboard/src/app/login/page.tsx
+++ naji-dashboard/src/app/login/page.tsx
@@ -1,3 +1,140 @@
+// 'use client';
+// import Image from 'next/image';
+// import React from 'react';
+// import { useState } from 'react';
+// import { useRouter } from 'next/navigation';
+// import { useForm } from 'react-hook-form';
+// import { yupResolver } from '@hookform/resolvers/yup';
+// import * as yup from 'yup';
+// import userlogin from "../utils/userlogin"
+
+// import { FiEye, FiEyeOff } from 'react-icons/fi';
+
+// const schema = yup.object().shape({
+//   email: yup.string().email('Invalid email').required('Email is required'),
+//   password: yup.string().required('Password is required'),
+// });
+
+// type FormData = yup.InferType<typeof schema>;
+
+// const Login = () => {
+//   const router = useRouter();
+//   const { register, handleSubmit, formState: { errors, isSubmitting } } = useForm<FormData>({
+//     resolver: yupResolver(schema),
+//   });
+
+//   const [apiError] = useState<string | null>(null);
+//   const [successMessage, setSuccessMessage] = useState<string | null>(null);
+//   const [passwordVisible, setPasswordVisible] = useState(false);
+
+//   const onSubmit = async (data: FormData) => {
+//     console.log('Login submitted', data);
+//     const response = await userlogin(data);
+//     if (response){
+//       setSuccessMessage("Logged in successfully! Let's go to your page .....")
+//       setTimeout(() => router.push("./homepage"), 1500)
+//     }
+//   };
+
+//   const togglePasswordVisibility = () => {
+//     setPasswordVisible(!passwordVisible);
+//   };
+
+//   return (
+//     <div className="flex h-screen">
+//       <div className="w-1/2 bg-[#f5f0e6] p-8 flex items-center justify-center">
+//         <div className="w-full ml-20">
+//           <h2 className="text-3xl mt-[-20px] font-extrabold mb-6 text-center">LogIn</h2>
+//           <form onSubmit={handleSubmit(onSubmit)} className="space-y-20">
+//             <div>
+//               <label htmlFor="email" className="block mb-2">
+//                 Email
+//               </label>
+//               <input
+//                 type="text"
+//                 id="email"
+//                 {...register('email')}
+//                 className="w-[80%] py-5 pl-6 border-2 border-[#641414] rounded-[25px]"
+//               />
+//               {errors.email && <p className="text-red-500 mt-1">{errors.email.message}</p>}
+//             </div>
+//             <div>
+//               <label htmlFor="password" className="block mb-2">
+//                 Password
+//               </label>
+//               <div className="relative">
+//                 <input
+//                   type={passwordVisible ? "text" : "password"}
+//                   id="password"
+//                   {...register('password')}
+//                   className="w-[80%] py-5 pr-10  pl-6 border-2 border-[#641414] rounded-[25px]"
+//                 />
+//                 <button
+//                   type="button"
+//                   onClick={togglePasswordVisibility}
+//                   className="absolute right-[22%] top-1/2 transform -translate-y-1/2"
+//                 >
+//                   {passwordVisible ? <FiEye /> : <FiEyeOff />}
+//                 </button>
+//               </div>
+//               {errors.password && <p className="text-red-500 mt-1">{errors.password.message}</p>}
+//             </div>
+//             <button
+//               type="submit"
+//               className={`w-44 mt-20 ml-[28%] flex justify-center bg-[#5b2a14] text-white py-5 rounded-[25px] hover:bg-[#ef5b1c]"
+//                 ${isSubmitting ? "opacity-40 cursor-not-allowed" : ""
+  
+//                 }`}
+  
+//                 disabled = {isSubmitting}
+//               >
+//                 {isSubmitting ? "Logging you In...." : "Login"}
+//             </button>
+
+
+//             {successMessage && (
+//               <p className="mt-2 text-green-500 text-sm ml-48">
+//                 {successMessage}
+//               </p>
+//             )}
+//            {apiError && (
+//               <p className="mt-2 text-red-500 text-center text-sm">
+//                 {apiError}
+//               </p>
+//             )}
+
+//           </form>
+//           <div className="mt-8 text-1xl ml-80">
+//             <span className="text-1xl">OR</span>
+//           </div>
+//           <button
+//             className="mt-4 w-80 py-5 ml-40 border-2 border-[#641414] rounded-[25px] flex items-center justify-center space-x-2 hover:bg-gray-50"
+//           >
+//             <Image src="/images/google.png" alt="Google logo" width={20} height={20} />
+//             <span className='w-[-80%] rounded-[25px]'>Continue with Google</span>
+//           </button>
+//           <p className="mt-16 ml-48 text-1xl">
+//             Don&#39;t have an account?{' '}
+//             <a onClick={() => router.push('/signup')} className="text-[#6b1d1d] hover:underline cursor-pointer">
+//               Sign Up
+//             </a>
+//           </p>
+//         </div>
+//       </div>
+//       <div className="w-1/2 h-dvh relative">
+//         <Image
+//           src="/images/Naji.jpg"
+//           alt="Coffee beans background"
+//           layout="fill"
+//           objectFit="cover"
+//         />
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Login;
+
 'use client';
 import Image from 'next/image';
 import React from 'react';
@@ -32,7 +169,7 @@ const Login = () => {
     const response = await userlogin(data);
     if (response){
       setSuccessMessage("Logged in successfully! Let's go to your page .....")
-      setTimeout(() => router.push("/Home"), 1500)
+      setTimeout(() => router.push('./home'), 1500)
     }
   };
 
@@ -134,7 +271,6 @@ const Login = () => {
 }
 
 export default Login;
-
 
 
 
--- naji-dashboard/src/app/utils/types.ts
+++ naji-dashboard/src/app/utils/types.ts
@@ -3,3 +3,18 @@ export interface FarmerDetails{
     farmer_phone_number?: string,
     farmer_county?:string
 }
+export interface PestIncident {
+    incident_id: number;
+    detection_date: string;
+    confidence_score: number;
+    affected_area_percentage: number;
+       
+   
+    }
+    export interface Pest {
+    pest_id: number;
+    pest_description: string;
+    pest_name: string;
+       
+    }
+ 
